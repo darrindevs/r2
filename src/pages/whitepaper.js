778b938f4c01d9ea3593b8f745ce1b5f1857e701
Zaino <hello@darrin.dev>
2022-03-25 14:31:28 -0700

    misc small updates

---
+++ src/pages/whitepaper.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Container } from 'react-bootstrap'
+import { Link } from "gatsby"
+
+// Step 2: Define your component
+const Whitepaper = () => {
+  return (
+    <main>
+      <h1>Whitepaper</h1>
+      
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default Whitepaper
