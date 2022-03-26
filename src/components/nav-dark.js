721c16a61af8d0fb4e0c8ab51be763fc3a0b01c1
Zaino <hello@darrin.dev>
2022-03-25 22:33:07 -0700

    fix home link

--- src/components/nav-dark.js
+++ src/components/nav-dark.js
@@ -5,12 +5,14 @@ import { Link } from "gatsby"
 
 const NavDark = (
     <Navbar bg="" expand="lg" className="navbar-dark">
-      <Navbar.Brand href="#home" className="ms-3 brando">1140nft</Navbar.Brand>
+      <Navbar.Brand href="/" className="ms-3 brando">1140nft</Navbar.Brand>
       <div id="nav-launch" className="mt-3 ms-2"><h6>Launching Summer 2022</h6></div>
       <Navbar.Toggle className="me-3 custom-toggler"/>
       <Navbar.Collapse className="justify-content-end me-3">
         <Navbar.Text className="text-white ms-3 nav-right">
-        <Link to="/whitepaper">Whitepaper</Link>
+        <Link className="me-3" to="/about">About</Link>
+        <Link className="me-3" to="/partnerships">Partnerships</Link>
+        <Link className="me-3" to="/whitepaper">Whitepaper</Link>
         </Navbar.Text>
       </Navbar.Collapse>
   </Navbar>
