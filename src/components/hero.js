6c871a9150c3b2f9353d886a534c635192041bd3
Zaino <hello@darrin.dev>
2022-03-28 21:13:24 -0700

    update hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,10 +2,8 @@ import * as React from "react"
 
 const Hero = () => (
     <div id="hero" className="container-component mt-5">
-      {/*<h1 className="1140">1140nft</h1>*/}
-      <h3 id="tagline" className='mb-3 mt-3'>The World's First NFT Funded<br></br> 
-      Real Estate Development Project</h3>
-      <h2 className="mt-3 mb-3" id="introducing"><i>Introducing</i><span> 1140nft</span></h2>
+      <h1 className="1140">1140nft</h1>
+                <h2 id="tagline" className='mb-3 mt-3'><span>The World's First NFT Funded</span><br></br><span>Real Estate Development</span> </h2>
       <p className="mb-4">1140nft is a groundbreaking NFT project that shares the value of IRL real estate with its holders. The NFT will fund the development of a small-lot subdivision in Los Angeles and create exponential growth. 1140nft holders share rental revenues, sales proceeds and member access to properties for LA event accomodations.
       </p>
       <h5 className="mt-3 mb-5">* Coming Summer 2022 *</h5>
