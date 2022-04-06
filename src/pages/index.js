c4d4b92efcbb5fc8cb3a4391e7186909126759ba
Zaino <hello@darrin.dev>
2022-04-06 15:38:20 -0700

    add hero mobile, update media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from "../components/hero"
+import HeroM from "../components/hero-m"
 import HeroImg from "../components/heroimg"
 //import Box1 from "../components/box1"
 //import Box2 from "../components/box2"
@@ -15,6 +16,7 @@ import Location from "../components/location"
 import LocationM from "../components/location-m"
 //import Timeline from "../components/timeline"
 import Cta2 from "../components/cta2"
+import CtaM from "../components/cta-m"
 import Formspree from "../components/formspree"
 
 
@@ -24,11 +26,13 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
+    <HeroM />
     <Ltv />
     <LtvM />
     <Location />
     <LocationM />
     <Cta2 />
+    <CtaM />
   </Layout>
 )
 
