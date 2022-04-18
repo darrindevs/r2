7c8f2ed7b4360734b2bba9cb52ff3fd4e5e243ce
Zaino <hello@darrin.dev>
2022-04-18 09:55:29 -0700

    add first blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,12 +8,49 @@ const BlogPage = () => (
   <Layout>
     <Seo title="Blog" />
     <main>
-        <h2>Blog</h2>
-        <div id="blog" className="container-component mt-5">
-            <p>The <span className="eleven-forty">1140nft</span> Blog is coming soon.</p>
-            <p>In the meantime follow us on Twitter.</p>
-          <p><button type="button" className="btn btn-red mt-2"><a className="btn-red" href='https://twitter.com/1140nft' target="blank">Follow</a></button></p>
-        </div>
+        <h2 className="pb-3">Introducing 1140nft</h2>
+        <p><i>April 18, 2022</i></p>
+        <h4 className="pb-3 pt-2">Too Small to Succeed</h4>
+        <p>
+          A little more than halfway through the meeting the banker hit me with the news. "It makes sense to develop this property, but it doesn't make sense for you to develop this property." He added: "The bank will not let you tear down their asset. Even if you have a better idea. Did you know that?" I didn't. So I owned the house and land, but I wouldn't be allowed to develop it.
+        </p>
+        <p>
+        Apparently a great idea was no match for the hurdles of traditional financing.
+        </p>
+        <p>
+        It seems that the trouble with developing my property is that it's too small for the big companies to get involved with… "We don't do less than one hundred units" is one I've heard from very capable developers along this path.
+        </p>
+        <p>
+        On the other hand the project is too big for an owner/developer to do on their own. Projects like mine have huge potential, but by the time someone like me secures all the financing for a project like this it becomes infeasible.
+        </p>
+        <p>
+        These were my results of not one, but two feasibility studies. I heard this so often that I started to believe it. Maybe I was out of my element.
+        </p>
+        <p>
+        But I knew how much potential my property held. It was perfect for sub-division and small unit development. I studied the county code front to back and back to front. My plan was solid and I knew I could obtain the permits. However, I couldn't secure traditional financing from the bank.
+        </p>
+        <h4 className="pt-5 pb-3">Too Valuable Not to Try</h4>
+        <p>
+        Fast forward to 2022 and the solution finally presents itself: <span><strong><i>a real estate development funded by an NFT offering</i></strong></span>.
+        </p>
+        <p>
+        Everyone agreed that it was an ideal property for development, but the banks weren't going to play ball with me. And so it struck me: why not use NFTs to fund my project?
+        </p>
+        <p>
+        The project is feasible on this scale because the property has so much potential. We can sell a limited amount of NFTs to individuals and fund the project. Then, we return profits back to the holders of the NFT for the duration of ownership.
+        </p>
+        <p>
+        People are looking to invest in innovative NFTs with real-world value. Real estate development has never been combined with the speculative interest NFTs bring. I believe this concept has real potential to build a strong and loyal community of investors and provide a solid return on investment.
+        </p>
+        <h4 className="pt-5 pb-3">The Journey Begins</h4>
+        <p>
+        So this is my story. A guy with a dream who was told no by the banks and decided to try something different. Come with me on this journey as I share the story of creating the world's first NFT funded real estate project.
+        </p>
+        <p>
+        <strong>1140nft is launching soon</strong>. Follow us on <a href="https://twitter.com/1140nft" target="blank">Twitter</a> to find out more.
+        </p>
+        <p className="mb-5">By Bradley Pierce · Creator / Co-Founder</p>
+        
     </main>
   </Layout>
 )
